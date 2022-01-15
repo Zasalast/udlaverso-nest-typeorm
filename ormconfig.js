1068b57935aededf8fa6548409d9f6efe8d81a5a
DesarrolloColectivo <zasalastobon@gmail.com>
2022-01-15 10:25:52 -0500

    example deploy heroku

--- ormconfig.js
+++ ormconfig.js
@@ -1,12 +1,11 @@
 module.exports = {
   type: 'postgres',
-  url: 'postgres://user:password@host:port/database',
+  url: process.env.DATABASE_URL,
   synchronize: false,
   logging: false,
   migrations: ['src/database/migrations /*.ts'],
   migrationsTableName: 'migrations',
-  entities: ['src/**/*.entity.ts'
-  ],
+  entities: ['src/**/*.entity.ts'],
   cli: {
     migrationsDir: 'src/database/migrations',
   },
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -37,8 +37,7 @@ const API_KEY_PROD = 'PROD1212121SA';
       provide: 'API_KEY',
       useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
     },
-
   ],
   exports: ['API_KEY', TypeOrmModule],
 })
-export class DatabaseModule { }
+export class DatabaseModule {}
